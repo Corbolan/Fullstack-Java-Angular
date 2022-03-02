e98496ebef9613451f35a37a35c4d61e4dd642f0
Germano Corbolan <36940585+Corbolan@users.noreply.github.com>
2022-03-01 21:57:08 -0300

    Tratamento de Exceção para CPF ja cadastrado

--- API/os/src/main/java/com/germano/os/services/TecnicoService.java
+++ API/os/src/main/java/com/germano/os/services/TecnicoService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.germano.os.domain.Tecnico;
 import com.germano.os.dtos.TecnicoDTO;
 import com.germano.os.repositories.TecnicoRepository;
+import com.germano.os.resource.exceptions.DataIntegratyViolationException;
 import com.germano.os.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,16 +17,30 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! " + id + ", Tipo: " + Tecnico.class.getName()));
 	}
+
 	public List<Tecnico> findAll() {
 		return repository.findAll();
 	}
+
 	public Tecnico create(TecnicoDTO objDTO) {
-		return repository.save( new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
-		
+		if (findByCPF(objDTO) != null) {
+			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+		}
+		return repository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
+
+	}
+
+	private Tecnico findByCPF(TecnicoDTO objDTO) {
+		Tecnico obj = repository.findByCPF(objDTO.getCpf());
+		if (obj != null) {
+			return obj;
+		}
+		return null;
 	}
 }
